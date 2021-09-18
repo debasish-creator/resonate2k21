1304ba3254c7b62aa35c88e27e0e0964e5b5a1f1
Gourab-18 <gourabomm2001@gmail.com>
2021-09-18 21:24:38 +0530

    Widecard design changed

--- src/components/WideCards/WideCards.js
+++ src/components/WideCards/WideCards.js
@@ -1,12 +1,15 @@
 import React from "react";
 import styled from "styled-components";
 import WideCard from "./WideCard";
+import { Link } from "react-router-dom";
 const Widecards = () => {
   return (
     <>
       <WidecardContainer>
-        <WideCard />
-        {/* <WideCard /> */}
+        <Link to="/journal">
+          <WideCard />
+          {/* <WideCard /> */}
+        </Link>
       </WidecardContainer>
     </>
   );
@@ -15,12 +18,14 @@ const Widecards = () => {
 export default Widecards;
 
 const WidecardContainer = styled.div`
-  height: 100vh;
+  height: 50vh;
   width: 100vw;
 
   background-color: #081733;
   display: flex;
   align-items: center;
   justify-content: center;
+  /* display: grid;
+  place-items: center; */
   flex-direction: column;
 `;
