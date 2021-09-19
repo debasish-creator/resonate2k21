f3ba3ae50accd7bffe766565beb17429b04f2474
Akash <akashsahu006@gmail.com>
2021-09-19 18:25:12 +0530

    Final

--- src/components/Carousel/carouselComponent.js
+++ src/components/Carousel/carouselComponent.js
@@ -1,52 +1,22 @@
 import React from "react";
 import { Carousel } from "react-carousel-minimal";
-
 import styled from "styled-components";
+import img from "./img1.jpg";
+
 const data = [
   {
     image:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0c/GoldenGateBridge-001.jpg/1200px-GoldenGateBridge-001.jpg",
+      "https://res.cloudinary.com/dfftvyqha/image/upload/v1632055392/rect833_y1bvhr.png",
     // caption: "San Francisco",
   },
   {
     image:
-      "https://cdn.britannica.com/s:800x450,c:crop/35/204435-138-2F2B745A/Time-lapse-hyper-lapse-Isle-Skye-Scotland.jpg",
+      "https://res.cloudinary.com/dfftvyqha/image/upload/v1632055453/rect867_j9g6vo.png",
     // caption: "Scotland",
   },
   {
     image:
-      "https://static2.tripoto.com/media/filter/tst/img/735873/TripDocument/1537686560_1537686557954.jpg",
-    // caption: "Darjeeling",
-  },
-  {
-    image:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/1/16/Palace_of_Fine_Arts_%2816794p%29.jpg/1200px-Palace_of_Fine_Arts_%2816794p%29.jpg",
-    // caption: "San Francisco",
-  },
-  {
-    image:
-      "https://i.natgeofe.com/n/f7732389-a045-402c-bf39-cb4eda39e786/scotland_travel_4x3.jpg",
-    // caption: "Scotland",
-  },
-  {
-    image:
-      "https://www.tusktravel.com/blog/wp-content/uploads/2020/07/Best-Time-to-Visit-Darjeeling-for-Honeymoon.jpg",
-    // caption: "Darjeeling",
-  },
-  {
-    image:
-      "https://www.omm.com/~/media/images/site/locations/san_francisco_780x520px.ashx",
-    // caption: "San Francisco",
-  },
-  {
-    image:
-      "https://images.ctfassets.net/bth3mlrehms2/6Ypj2Qd3m3jQk6ygmpsNAM/61d2f8cb9f939beed918971b9bc59bcd/Scotland.jpg?w=750&h=422&fl=progressive&q=50&fm=jpg",
-    // caption: "Scotland",
-  },
-  {
-    image:
-      "https://www.oyorooms.com/travel-guide/wp-content/uploads/2019/02/summer-7.jpg",
-    // caption: "Darjeeling",
+      "https://res.cloudinary.com/dfftvyqha/image/upload/v1632055497/rect5090_tkxjrx.png",
   },
 ];
 function carouselComponent() {
